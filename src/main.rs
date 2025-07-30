7bba00b60470c89cadbbf18f2efdc22b3e872609
likanug <likanug.g@qq.com>
2025-07-30 16:54:18 +0800

    使用use_resource函数的背压机制解决异步请求中发起请求过快(按钮点击过快)，无法及时处理请求响应的问题

--- src/main.rs
+++ src/main.rs
@@ -38,30 +38,23 @@ struct DogApi {
 
 #[component]
 fn DogView() -> Element {
-    
-    let mut img_src = use_signal(|| "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg".to_string());
-
-    let fetch_new = move |_| async move {
-        let response = reqwest::get("https://dog.ceo/api/breeds/image/random")
+    let mut img_src = use_resource(|| async move {
+        reqwest::get("https://dog.ceo/api/breeds/image/random")
             .await
             .unwrap()
             .json::<DogApi>()
             .await
-            .unwrap();
+            .unwrap()
+            .message
+    });
 
-        img_src.set(response.message);
-    };
-
-    let save = move |evt| {
-        info!("Clicked save ! Event: {evt:?}");
-    };
     rsx! {
         div { id: "dogview",
-            img { src: "{img_src}" }
+            img { src: img_src.cloned().unwrap_or_default() }
         }
         div { id: "buttons",
-            button { onclick: fetch_new, id: "skip", "skip" }
-            button { onclick: save, id: "save", "save!" }
+            button { onclick: move |_| img_src.restart(), id: "skip", "skip" }
+            button { onclick: move |_| img_src.restart(), id: "save", "save!" }
         }
     }
 }
