d29a1dc72edfaa1b1e25e8ec16a7ccec3873c019
likanug <likanug.g@qq.com>
2025-07-30 20:06:02 +0800

    添加favicon

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use components::{DogView, Favorites, NavBar};
 use dioxus::prelude::*;
 
 const MAIN_CSS: Asset = asset!("/assets/main.css");
+const FAVICON: Asset = asset!("/assets/favicon.ico");
 
 #[derive(Routable, PartialEq, Clone)]
 enum Route {
@@ -22,6 +23,7 @@ fn main() {
 
 fn app() -> Element {
     rsx! {
+        document::Link { rel: "icon", href: FAVICON }
         document::Stylesheet { href: MAIN_CSS }
         Router::<Route> {}
     }
